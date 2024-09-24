0d068af4fa686238220404bc85233ed85c989390
Cr-Israel <carlosisrael08@hotmail.com>
2024-09-24 17:21:54 -0300

    feat: EnvModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,7 +4,8 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { HttpModule } from './http/http.module';
 
-import { envSchema } from './env';
+import { envSchema } from './env/env';
+import { EnvModule } from './env/env.module';
 
 
 @Module({
@@ -14,6 +15,7 @@ import { envSchema } from './env';
   }),
     AuthModule,
     HttpModule,
+    EnvModule
   ],
 })
 export class AppModule { }
